cffc843301bcaa95ee8804c1ca5ce9ed6faa97ab
heapshero <heaphero@prorobotech.ru>
2026-03-01 20:42:32 +0300

    Feature cloud 432 ns and ag (#1)
    Added API definitions for Namespace and AddressGroups resources

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# sgroups-proto
+# SGroups API
+
+Protocol Buffers definitions and generated code for the **SGroups** API.
+
+Generated artifacts are written to `pkg/api/` (Go stubs and OpenAPI/Swagger JSON).
+
+You do **not** need to install `protoc` or `protoc-gen-*` plugins manually.
+
+## Quick Start
+
+Generate Go code + Swagger/OpenAPI JSON:
+
+`make generate-api`
+
+Run tests:
+
+`make test`
+
+## Swagger/OpenAPI
+
+Swagger JSON files are generated into `pkg/api/**.swagger.json` and embedded by the Go package under `pkg/`.
---
+++ pkg/swagger-utils.go
@@ -0,0 +1,84 @@
+package pkg
+
+import (
+	"embed"
+	"encoding/json"
+	"io"
+	"reflect"
+
+	sgroupsv1 "github.com/PRO-Robotech/sgroups-proto/pkg/api/sgroups/v1"
+
+	"github.com/go-openapi/spec"
+	"github.com/pkg/errors"
+)
+
+var (
+	//go:embed api/*/*.swagger.json api/*/*/*.swagger.json
+	swaggerStore embed.FS
+)
+
+var (
+	//ErrSwaggerNotExist when document is not found
+	ErrSwaggerNotExist = errors.New("swagger doc is no exist")
+)
+
+// SwaggerUtil ...
+type SwaggerUtil[T any] struct{}
+
+func (u SwaggerUtil[T]) reg(p string) {
+	swaggerPaths[reflect.TypeOf((*T)(nil)).Elem()] = p
+}
+
+// GetSpec ...
+func (u SwaggerUtil[T]) GetSpec() (*spec.Swagger, error) {
+	res := new(spec.Swagger)
+	ty := reflect.TypeOf((*T)(nil)).Elem()
+	p := swaggerPaths[ty]
+	err := whenFindSwagger(p, func(r io.Reader) error {
+		return json.NewDecoder(r).Decode(res)
+	})
+	return res, err
+}
+
+// GetRaw ...
+func (u SwaggerUtil[T]) GetRaw() (json.RawMessage, error) {
+	var ret json.RawMessage
+	ty := reflect.TypeOf((*T)(nil)).Elem()
+	p := swaggerPaths[ty]
+	err := whenFindSwagger(p, func(reader io.Reader) error {
+		data, e := io.ReadAll(reader)
+		ret = data
+		return e
+	})
+	return ret, err
+}
+
+var (
+	swaggerPaths = make(map[reflect.Type]string)
+)
+
+func whenFindSwagger(p string, f func(reader io.Reader) error) error {
+	data, e := swaggerStore.Open(p)
+	if e != nil {
+		return ErrSwaggerNotExist
+	}
+	return f(data)
+}
+
+func init() {
+	const (
+		apiSGroups = "api/sgroups/v1/services.swagger.json"
+	)
+
+	apis := [...]interface{ reg(string) }{
+		SwaggerUtil[sgroupsv1.SGroupsNamespaceAPIServer]{},
+		SwaggerUtil[sgroupsv1.SGroupsNamespaceAPIClient]{},
+
+		SwaggerUtil[sgroupsv1.SGroupsAddressGroupsAPIServer]{},
+		SwaggerUtil[sgroupsv1.SGroupsAddressGroupsAPIClient]{},
+	}
+
+	for _, api := range apis {
+		api.reg(apiSGroups)
+	}
+}
